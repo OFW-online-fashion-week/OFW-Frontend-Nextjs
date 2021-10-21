69f6a2851eaf39e5d51dd2e9bc1554a48608fee0
jiwon <jidole041214@naver.com>
2021-10-21 11:41:16 +0900

    update

--- components/collection-list/CollectionColumnCard.tsx
+++ components/collection-list/CollectionColumnCard.tsx
@@ -1,5 +1,7 @@
 import * as S from "./styles";
 import { CollectionCard } from "./../../lib/interfaces/collection";
+import { useRouter } from "next/dist/client/router";
+import { useCallback } from "react";
 
 interface Props extends CollectionCard {
   isAdd?: boolean;
@@ -12,12 +14,22 @@ export default function CollectionColumnCard({
   id,
   isAdd,
 }: Props) {
+  const router = useRouter();
+
+  const routingToCollection = useCallback(() => {
+    router.push(`/collections/${id}`);
+  }, []);
+
   return (
     <S.ColumnCard>
       {isAdd ? (
         <button className="add-wrap">+</button>
       ) : (
-        <img src={coverImg} className="cover-img" />
+        <img
+          src={coverImg}
+          onClick={routingToCollection}
+          className="cover-img"
+        />
       )}
       <h1 className="collection-title">{title}</h1>
       <p className="collection-description">
