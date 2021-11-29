191bf0b72b96ff25c1091ed75589472e6d2bacd5
jiwon <jidole041214@naver.com>
2021-11-30 08:27:22 +0900

    .

--- components/brand-search/index.tsx
+++ components/brand-search/index.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./styles";
 import { useRouter } from "next/dist/client/router";
 
 export default function BrandSearch() {
+  const [spellArr, setSpellArr] = React.useState<string[]>([]);
+  const [nowSpell, setNowSpell] = useState<any>("a");
   const router = useRouter();
+  React.useEffect(() => {
+    let tmpArr = [];
+    for (let i = 97; i <= 122; i++) {
+      tmpArr.push(String.fromCharCode(i));
+    }
+    setSpellArr(tmpArr);
+  }, []);
   return (
     <S.Wrapper>
+      <S.SideWrap>
+        {spellArr.map((spell, index) => (
+          <button
+            key={index}
+            className={nowSpell === spell ? "active" : ""}
+            onClick={() => setNowSpell(spell)}
+          >
+            {spell}
+          </button>
+        ))}
+      </S.SideWrap>
       <S.Container>
-        <h1 className="title">A</h1>
+        <h1 className="title">
+          A<b>PART</b>
+        </h1>
         <S.BrandContainer>
           <span onClick={() => router.push("/brands/1")}>adder error</span>
           <span onClick={() => router.push("/brands/1")}>adder error</span>
--- components/brand-search/styles.ts
+++ components/brand-search/styles.ts
@@ -1,7 +1,36 @@
 import styled from "@emotion/styled";
-import { BaseContainer, BaseWrapper } from "../../styles";
+import { BaseContainer, BaseWrapper, COLOR } from "../../styles";
 
 export const Wrapper = styled(BaseWrapper)``;
+
+export const SideWrap = styled.nav`
+  position: absolute;
+  width: 130px;
+  left: 0;
+  margin-left: 130px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-row-gap: 20px;
+  & button {
+    color: black;
+    font-size: 24px;
+    text-transform: uppercase;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  & .active {
+    color: ${COLOR.main};
+    font-weight: bold;
+    &::before {
+      content: "⬝";
+      color: ${COLOR.main};
+      position: absolute;
+      margin-right: 40px;
+      margin-bottom: 3px;
+    }
+  }
+`;
 
 export const Container = styled(BaseContainer)`
   display: flex;
@@ -10,6 +39,10 @@ export const Container = styled(BaseContainer)`
     font-size: 200px;
     font-weight: 700;
     text-transform: uppercase;
+    & b {
+      font-size: 50px;
+      margin-left: 50px;
+    }
   }
 `;
 
